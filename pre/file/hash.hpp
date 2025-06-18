f7da7db9cb1b5245fa92d2e2b253c04a4a708b1b
Florent Castelli <florent@engflow.com>
2025-06-18 14:47:49 +0200

    Upgrade to boost 1.89.0
    Boost 1.89.0 is required to have a compatible standard library.
    Older versionis would use std::unary_function which is not supported anymore
    in C++17.
    
    Using the proper internal type name boost::uuids::detail::sha1::digest_type
    result in 2 different behaviors depending on the Boost version.
    The type can either be an array of "unsigned int[5]" (older Boost) or
    "char[20]" (newer Boost) which the serialization code needs to handle.
    
    The serialization code is also moved away from using sprintf as modern
    compilers will emit lots of different warnings or errors.

--- pre/file/hash.hpp
+++ pre/file/hash.hpp
@@ -2,6 +2,7 @@
 
 #include <stdexcept>
 #include <system_error>
+#include <iterator>
 #include <fstream>
 #include <string>
 #include <vector>
@@ -9,6 +10,13 @@
 #include <boost/uuid/detail/sha1.hpp>
 
 namespace pre::file {
+
+  namespace {
+    constexpr char int_to_hex[16] = {
+      '0', '1', '2', '3', '4', '5', '6', '7', '8',
+      '9', 'a', 'b', 'c', 'd', 'e', 'f'
+    };
+  }
 
   using namespace std::string_literals;
 
@@ -41,13 +49,31 @@ namespace pre::file {
     }
 
     // compute the hash
-    uint32_t hash[5] = {0};
+    boost::uuids::detail::sha1::digest_type hash = {0};
     char hash_buf[41] = {0};
 
     sha1.get_digest(hash);    
 
-    for (int i = 0; i < 5; i++) {
-      std::sprintf(hash_buf + (i << 3), "%08x", hash[i]);
+    if constexpr (std::size(hash) == 20) {
+      // Newer Boost has digest_type of type "unsigned char[20]"
+      for (size_t i = 0; i < std::size(hash); i++) {
+        hash_buf[i * 2 + 0] = int_to_hex[(hash[i] >> 4) & 0xF];
+        hash_buf[i * 2 + 1] = int_to_hex[(hash[i]     ) & 0xF];
+      }
+    } else if constexpr (std::size(hash) == 5) {
+      // Older Boost has digest_type of type "unsigned int[5]"
+      for (size_t i = 0; i < std::size(hash); i++) {
+        hash_buf[i * 2 + 0] = int_to_hex[(hash[i] >> 28) & 0xF];
+        hash_buf[i * 2 + 1] = int_to_hex[(hash[i] >> 24) & 0xF];
+        hash_buf[i * 2 + 2] = int_to_hex[(hash[i] >> 20) & 0xF];
+        hash_buf[i * 2 + 3] = int_to_hex[(hash[i] >> 16) & 0xF];
+        hash_buf[i * 2 + 4] = int_to_hex[(hash[i] >> 12) & 0xF];
+        hash_buf[i * 2 + 5] = int_to_hex[(hash[i] >>  8) & 0xF];
+        hash_buf[i * 2 + 6] = int_to_hex[(hash[i] >>  4) & 0xF];
+        hash_buf[i * 2 + 7] = int_to_hex[(hash[i]      ) & 0xF];
+      }
+    } else {
+      throw std::runtime_error("Unsupported boost::uuids::detail::sha1::digest_type size");
     }
 
     return std::string(hash_buf);
